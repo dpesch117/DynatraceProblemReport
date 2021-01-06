491f6f00bea2925a4a8f8e286766fb786a3b625d
Pesch <dallas.pesch@dynatrace.com>
2021-01-06 11:10:22 -0700

    imported package. included unfinished function apiRequest()

--- main.go
+++ main.go
@@ -2,9 +2,33 @@ package main
 
 import(
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Welcome to the Top problem report")
+	apiRequest()
+
+}
+
+func apiRequest(){
+
+	response, err := http.Get("")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil  {
+		log.Fatal(err)
+		}
+	
+	fmt.Println(string(responseData))
+
+
 }
