f7ca10944f4c7ca446389c323e8237d6d00c2c89
= <=>
2021-01-06 14:50:24 -0700

    workin on Data structures. Updated apiRequest function to read from config file

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import(
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
+
+type Response struct {
+	TotalCount int `json:"totalCount"`
+	PageSize int `json:"pageSize"`
+	//problems Problems `json:"problems"`
+}
+
+
+type Problems struct {
+	ProblemId string `json:"problemId"`
+	DisplayId string `json:"displayId"`
+	Title string `json:"title"`
+	ImpactLevel string `json:"impactLevel"`
+	SeverityLevel string `json:"severityLevel"`
+	Status string `json:"status"`
+	AffectedEntities affectedEntities `json:"affectedEntities"`
+}
+
+type affectedEntities struct {
+	EntityId entityId `json:"entityId"`
+}
+
+type entityId struct {
+
+}
+
+
+type Config struct {
+	TenantURL string `json:"tenantURL"`
+	ApiToken string `json:"Api-Token"`
+
+}
 
 func main() {
 	fmt.Println("Welcome to the Top problem report")
@@ -16,8 +49,20 @@ func main() {
 
 // apiRequest used to reach to the Dynatrace API and pull back response data as a json
 func apiRequest(){
+	//Reading data from configuration file 
+	configFile, err := ioutil.ReadFile("config.json")
+	//error handling for configuration file
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//create variable for Configurations
+	var config Config
+	//unmarshal the json into the config variable
+	json.Unmarshal(configFile, &config)
+
 	//variable for the GET request
-	response, err := http.Get("")
+	response, err := http.Get("https://" + config.TenantURL + ".live.dynatrace.com/api/v2/problems?Api-Token=" + config.ApiToken)
 	//if statement to determine if there is an error in the GET request. If there is an error then exit the program
 	if err != nil {
 		fmt.Print(err.Error())
@@ -29,8 +74,15 @@ func apiRequest(){
 	if err != nil  {
 		log.Fatal(err)
 		}
+
+	var responseObject Response
+
+
+	json.Unmarshal(responseData, &responseObject)
 	//print the responseData variable as a string
-	fmt.Println(string(responseData))
+	fmt.Println(responseObject.TotalCount)
+	fmt.Println(responseObject.PageSize)
+	//fmt.Println(string(responseData))
 
 
 }
