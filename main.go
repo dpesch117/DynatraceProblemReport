45fdd8ea2d47c76ad884129830c965cf972b1f27
Pesch <dallas.pesch@dynatrace.com>
2021-01-06 11:29:48 -0700

    adding comments

--- main.go
+++ main.go
@@ -14,20 +14,22 @@ func main() {
 
 }
 
+// apiRequest used to reach to the Dynatrace API and pull back response data as a json
 func apiRequest(){
-
+	//variable for the GET request
 	response, err := http.Get("")
-
+	//if statement to determine if there is an error in the GET request. If there is an error then exit the program
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
-
+	//variable to map response data 
 	responseData, err := ioutil.ReadAll(response.Body)
+	//if statement to determine if the error variable is empty. If empty then log error 
 	if err != nil  {
 		log.Fatal(err)
 		}
-	
+	//print the responseData variable as a string
 	fmt.Println(string(responseData))
 
 
