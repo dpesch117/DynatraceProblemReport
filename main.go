9de02ae93880a313dcac9f37d051930c7bbf97b3
= <=>
2021-01-08 12:20:03 -0700

    Removed API-Token from URL Path and implemented it into the HTTP request header

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import(
 	"log"
 	"net/http"
 	"os"
-	"reflect"//reflect can be removed. It is purely for testing purposes
+	//"reflect"//reflect can be removed. It is purely for testing purposes
 )
 
 type Response struct {
@@ -60,10 +60,15 @@ type Config struct {
 
 func main() {
 	fmt.Println("Welcome to the Top problem report")
+
+	//assigning return of apiRequest() function to "response" variable
 	response := apiRequest()
+	
 	fmt.Println("printing Json")
-	fmt.Println(reflect.TypeOf(response))
-	fmt.Println(response)
+
+	//calling ParseJSON function and passing response of apirequest() as a parameter
+	parseJSON(response)
+	//fmt.Println(response)
 
 }
 
@@ -82,31 +87,33 @@ func apiRequest()(Response){
 	json.Unmarshal(configFile, &config)
 
 	//variable for the GET request
-	response, err := http.Get("https://" + config.TenantURL + ".live.dynatrace.com/api/v2/problems?Api-Token=" + config.ApiToken)
+	request, err := http.NewRequest("GET", "https://" + config.TenantURL + ".live.dynatrace.com/api/v2/problems", nil)
 	//if statement to determine if there is an error in the GET request. If there is an error then exit the program
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
-	//variable to map response data 
-	responseData, err := ioutil.ReadAll(response.Body)
+	request.Header.Set("Authorization", "Api-Token " + config.ApiToken)
+
+	//Variable for the Response
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+
+	//variable to map response body 
+	responseBody, err := ioutil.ReadAll(response.Body)
 	//if statement to determine if the error variable is empty. If empty then log error 
 	if err != nil  {
 		log.Fatal(err)
 		}
 
 	var responseObject Response
-//testing for loop to iterate over response data
-/*
-	for i:= range responseObject.Problems{
-
-		fmt.Println(responseObject.Problems[i].ManagementZones)
-
-	}
-*/
 
 	//unmartial the responseData Json payload and assign to the responseObject variable
-	json.Unmarshal(responseData, &responseObject)
+	json.Unmarshal(responseBody, &responseObject)
 	//print the responseData variable as a string
 	//fmt.Println(string(responseData))
 
@@ -114,4 +121,5 @@ func apiRequest()(Response){
 	return responseObject
 
 }
+
 
